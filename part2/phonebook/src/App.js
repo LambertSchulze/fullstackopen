629496b0591e09c117ee565419cc545160d5d247
Lambert Schulze <schulze.lambert@googlemail.com>
2022-02-14 21:22:54 +0100

    phonebook step5

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-123456'
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -33,29 +34,43 @@ const App = () => {
     setNewNumber('')
   }
 
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+
+      <label>search
+        <input type="search" value={searchTerm} onChange={handleSearchChange} />
+      </label>
+
+      <h2>new Contact</h2>
       <form onSubmit={handleSubmit}>
         <div>
           <label>name
-            <input value={newName} onChange={handleNameChange} required />
+            <input type="text" value={newName} onChange={handleNameChange} required pattern="[a-Z]" placeholder="Mike Wazowski"/>
           </label>
         </div>
         <div>
           <label>number
-            <input value={newNumber} onChange={handleNumberChange} required />
+            <input type="tel" value={newNumber} onChange={handleNumberChange} required placeholder="+49-30-12345678" />
           </label>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+      
       <h2>Numbers</h2>
         <ul>
-          {persons.map(p => 
-            <li key={p.name}>{p.name} {p.number}</li>  
-          )}
+          {persons
+            .filter(p => p.name.toLowerCase().includes(searchTerm.toLowerCase()))
+            .map((p, index) => 
+              <li key={index}>{p.name} {p.number}</li>  
+            )
+          }
         </ul>
     </div>
   )
